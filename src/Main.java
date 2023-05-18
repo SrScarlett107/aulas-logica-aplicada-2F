c9f30d51a9e7dea5ef6fad819ddc2d6408d2640e
SrScarlett107 <edu25070@gmail.com>
2023-05-18 08:25:52 -0300

    adão é um power ranger

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Aula de Orientação á objetos
+        // Declarar objetos
+        Pessoa adao;
+        // instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+        // Adicionar comportamento
+        adao.falar();
+        eva.falar();
+
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    // Atributos da classe
+    public String nome;
+    public String sobrenome;
+    // Método da classe
+    public void falar(){
+        System.out.println("Falei");
+    }
+}
